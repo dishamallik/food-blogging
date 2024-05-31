21d49cbd05e9489302e8ff590f046e845efb65a7
dishamallik <dishamallik758@gmail.com>
2024-05-31 21:53:34 +0600

    creating simple website

--- src/Pages/AddBlog.jsx
+++ src/Pages/AddBlog.jsx
@@ -1,138 +1,172 @@
-import { useEffect, useState } from "react";
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
+import axios from 'axios';
 import Swal from 'sweetalert2';
-import useAuth from "../hook/useAuth";
+import useAuth from '../hook/useAuth';
+
+const fetchUserData = async (user) => {
+  if (user) {
+    return {
+      email: user.email || '',
+      displayName: user.displayName || '',
+      photoURL: user.photoURL || ''
+    };
+  }
+  return { email: '', displayName: '', photoURL: '' };
+};
+
+const postBlog = async (newBlog) => {
+  const response = await axios.post('http://localhost:5000/blogs', newBlog, {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+  return response.data;
+};
 
 const AddBlog = () => {
-    const { user } = useAuth();
-    const [userEmail, setUserEmail] = useState("");
-    const [userName, setUserName] = useState("");
-    const [userPhotoURL, setUserPhotoURL] = useState("");
+  const { user } = useAuth();
+  const queryClient = useQueryClient();
 
-    useEffect(() => {
-        if (user) {
-            setUserEmail(user.email || "");
-            setUserName(user.displayName || "");
-            setUserPhotoURL(user.photoURL || "");
-        }
-    }, [user]);
+  const { data: userData } = useQuery({
+    queryKey: ['userData', user],
+    queryFn: () => fetchUserData(user),
+    enabled: !!user,
+  });
 
-    const handleAddBlog = async (e) => {
-        e.preventDefault();
-        const form = e.target;
-        const title = form.title.value;
-        const image = form.image.value;
-        const category = form.category.value;
-        const shortDescription = form.short_description.value;
-        const longDescription = form.long_description.value;
-    
-        if (!title || !image || !category || !shortDescription || !longDescription) {
-            Swal.fire({
-                title: 'Error!!',
-                text: 'Please fill in all the fields',
-                icon: 'error',
-                confirmButtonText: 'OK'
-            });
-            return;
-        }
-    
-        const newBlog = {
-            title,
-            image,
-            category,
-            shortDescription,
-            longDescription,
-            userEmail,
-            userName,
-            userPhotoURL  
-        };
-    
-        try {
-            const response = await fetch('http://localhost:5000/blogs', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(newBlog)
-            });
-    
-            const data = await response.json();
-    
-            if (data.insertedId) {
-                Swal.fire({
-                    title: 'Success!!',
-                    text: 'Blog Post Added Successfully',
-                    icon: 'success',
-                    confirmButtonText: 'Cool'
-                });
-    
-                // Clear form fields after successful submission
-                form.reset();
-                setUserEmail("");
-                setUserName("");
-                setUserPhotoURL("");
-            }
-        } catch (error) {
-            console.error('Error:', error);
-            Swal.fire({
-                title: 'Error!!',
-                text: 'Something went wrong. Please try again later.',
-                icon: 'error',
-                confirmButtonText: 'OK'
-            });
-        }
+  const mutation = useMutation({
+    mutationFn: postBlog,
+    onSuccess: (data) => {
+      if (data.insertedId) {
+        Swal.fire({
+          title: 'Success!!',
+          text: 'Blog Post Added Successfully',
+          icon: 'success',
+          confirmButtonText: 'Cool'
+        });
+
+        queryClient.invalidateQueries('blogs');
+      }
+    },
+    onError: () => {
+      Swal.fire({
+        title: 'Error!!',
+        text: 'Something went wrong. Please try again later.',
+        icon: 'error',
+        confirmButtonText: 'OK'
+      });
+    },
+  });
+
+  const handleAddBlog = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const title = form.title.value;
+    const image = form.image.value;
+    const category = form.category.value;
+    const shortDescription = form.short_description.value;
+    const longDescription = form.long_description.value;
+
+    if (!title || !image || !category || !shortDescription || !longDescription) {
+      Swal.fire({
+        title: 'Error!!',
+        text: 'Please fill in all the fields',
+        icon: 'error',
+        confirmButtonText: 'OK'
+      });
+      return;
     }
-    
-    return (
-        <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-            <div className="mt-10 mb-10">
-                <div className="bg-teal-100 p-6 sm:p-10 lg:p-24">
-                    <h2 className="text-3xl font-extrabold mb-6">Add Blog Post</h2>
-                    <form onSubmit={handleAddBlog}>
-                        {/* User Info Card */}
-                        <div className="bg-white p-4 sm:p-6 rounded-lg shadow-md mb-6">
-                            <div className="flex items-center">
-                                {userPhotoURL && <img src={userPhotoURL} alt="User Photo" className="w-12 h-12 sm:w-16 sm:h-16 rounded-full mr-4"/>}
-                                <div>
-                                    <p className="text-lg font-medium">{userName}</p>
-                                    <p className="text-sm text-gray-600">{userEmail}</p>
-                                </div>
-                            </div>
-                        </div>
 
-                        <div className="mb-4">
-                            <label className="block text-sm font-medium text-gray-700">Title:</label>
-                            <input type="text" name="title" className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
-                        </div>
-                        <div className="mb-4">
-                            <label className="block text-sm font-medium text-gray-700">Image URL:</label>
-                            <input type="text" name="image" className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
-                        </div>
-                        <div className="mb-4">
-                            <label className="block text-sm font-medium text-gray-700">Category:</label>
-                            <select name="category" className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
-                                <option value="">Select</option>
-                                <option value="Desserts">Desserts</option>
-                                <option value="Asian">Asian</option>
-                                <option value="Vegan">Vegan</option>
-                                <option value="Salad">Salad</option>
-                                <option value="Breakfast">Breakfast</option>
-                            </select>
-                        </div>
-                        <div className="mb-4">
-                            <label className="block text-sm font-medium text-gray-700">Short Description:</label>
-                            <textarea name="short_description" className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"></textarea>
-                        </div>
-                        <div className="mb-4">
-                            <label className="block text-sm font-medium text-gray-700">Long Description:</label>
-                            <textarea name="long_description" className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"></textarea>
-                        </div>
-                        
-                        <button type="submit" className="bg-indigo-600 text-white py-2 px-4 rounded-md">Add Post</button>
-                    </form>
+    const newBlog = {
+      title,
+      image,
+      category,
+      shortDescription,
+      longDescription,
+      userEmail: userData?.email,
+      userName: userData?.displayName,
+      userPhotoURL: userData?.photoURL,
+    };
+
+    mutation.mutate(newBlog);
+    form.reset();
+  };
+
+  return (
+    <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="mt-10 mb-10">
+        <div className="bg-teal-100 p-6 sm:p-10 lg:p-24">
+          <h2 className="text-3xl font-extrabold mb-6">Add Blog Post</h2>
+          <form onSubmit={handleAddBlog}>
+            {/* User Info Card */}
+            <div className="bg-white p-4 sm:p-6 rounded-lg shadow-md mb-6">
+              <div className="flex items-center">
+                {userData?.photoURL && (
+                  <img
+                    src={userData.photoURL}
+                    alt="User Photo"
+                    className="w-12 h-12 sm:w-16 sm:h-16 rounded-full mr-4"
+                  />
+                )}
+                <div>
+                  <p className="text-lg font-medium">{userData?.displayName}</p>
+                  <p className="text-sm text-gray-600">{userData?.email}</p>
                 </div>
+              </div>
             </div>
+
+            <div className="mb-4">
+              <label className="block text-sm font-medium text-gray-700">Title:</label>
+              <input
+                type="text"
+                name="title"
+                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-sm font-medium text-gray-700">Image URL:</label>
+              <input
+                type="text"
+                name="image"
+                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-sm font-medium text-gray-700">Category:</label>
+              <select
+                name="category"
+                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+              >
+                <option value="">Select</option>
+                <option value="Desserts">Desserts</option>
+                <option value="Asian">Asian</option>
+                <option value="Vegan">Vegan</option>
+                <option value="Salad">Salad</option>
+                <option value="Breakfast">Breakfast</option>
+              </select>
+            </div>
+            <div className="mb-4">
+              <label className="block text-sm font-medium text-gray-700">Short Description:</label>
+              <textarea
+                name="short_description"
+                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+              ></textarea>
+            </div>
+            <div className="mb-4">
+              <label className="block text-sm font-medium text-gray-700">Long Description:</label>
+              <textarea
+                name="long_description"
+                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+              ></textarea>
+            </div>
+
+            <button type="submit" className="bg-indigo-600 text-white py-2 px-4 rounded-md">
+              Add Post
+            </button>
+          </form>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default AddBlog;
--- src/Pages/Brand.jsx
+++ src/Pages/Brand.jsx
@@ -1,4 +1,5 @@
 import { FaArrowRight } from "react-icons/fa";
+import { motion } from "framer-motion";
 
 const Brand = () => {
     return (
@@ -8,30 +9,35 @@ const Brand = () => {
                 <div className="border-b-2 border-red-600 w-16 mx-auto mt-2"></div>
             </div>
             <div className="flex flex-wrap justify-center gap-4">
-                <div className="card w-72 sm:w-96 bg-base-100 shadow-xl transform transition-transform hover:scale-105 hover:shadow-2xl hover:bg-rose-200">
-                    <div className="card-body transition-colors duration-300">
-                        <h2 className="card-title text-1xl text-rose-800 flex items-center hover:text-rose-600">
-                            Explore Recipes <FaArrowRight className="ml-2" />  {/* Add the arrow icon here */}
-                        </h2>
-                    </div>
-                    <figure><img src="https://i.ibb.co/6nLhPDR/bon.webp" alt="Shoes" /></figure>
-                </div>
-                <div className="card w-72 sm:w-96 bg-base-100 shadow-xl transform transition-transform hover:scale-105 hover:shadow-2xl hover:bg-rose-200">
-                    <div className="card-body transition-colors duration-300">
-                        <h2 className="card-title text-rose-800 flex items-center hover:text-rose-600">
-                            Explore Recipes <FaArrowRight className="ml-2" />  {/* Add the arrow icon here */}
-                        </h2>
-                    </div>
-                    <figure><img src="https://i.ibb.co/zJTsPXP/Gourmet-Square.webp" alt="Hats" /></figure>
-                </div>
-                <div className="card w-72 sm:w-96 bg-base-100 shadow-xl transform transition-transform hover:scale-105 hover:shadow-2xl hover:bg-rose-200">
-                    <div className="card-body transition-colors duration-300">
-                        <h2 className="card-title text-rose-800 flex items-center hover:text-rose-600">
-                            Explore Recipes <FaArrowRight className="ml-2" />  {/* Add the arrow icon here */}
-                        </h2>
-                    </div>
-                    <figure><img src="https://i.ibb.co/MnL3NZR/Cookbook-Sqaure.webp" alt="Gloves" /></figure>
-                </div>
+                {[
+                    {
+                        imgSrc: "https://i.ibb.co/6nLhPDR/bon.webp",
+                        altText: "Bon Appetit"
+                    },
+                    {
+                        imgSrc: "https://i.ibb.co/zJTsPXP/Gourmet-Square.webp",
+                        altText: "Gourmet"
+                    },
+                    {
+                        imgSrc: "https://i.ibb.co/MnL3NZR/Cookbook-Sqaure.webp",
+                        altText: "Cookbook"
+                    }
+                ].map((brand, index) => (
+                    <motion.div
+                        key={index}
+                        className="card w-72 sm:w-96 bg-base-100 shadow-xl"
+                        whileHover={{ scale: 1.05, boxShadow: "0px 10px 30px rgba(0, 0, 0, 0.3)" }}
+                    >
+                        <div className="card-body transition-colors duration-300">
+                            <h2 className="card-title text-1xl text-rose-800 flex items-center hover:text-rose-600">
+                                Explore Recipes <FaArrowRight className="ml-2" />
+                            </h2>
+                        </div>
+                        <figure>
+                            <img src={brand.imgSrc} alt={brand.altText} />
+                        </figure>
+                    </motion.div>
+                ))}
             </div>
         </div>
     );
